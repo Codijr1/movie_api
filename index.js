5fcfb1272fe6c1f5cfb15dda7613abfdadc3027e
Codi <cjrobards@gmail.com>
2023-08-10 19:43:02 -0400

    prep

--- index.js
+++ index.js
@@ -1,10 +1,13 @@
-const express = require('express');
-const app = express();
-const http = require('http');
+const express= require('express');
+const app= express();
+const http= require('http');
+const morgan= require('morgan');
+const port= 8080;
 
-http.createServer((request, response) => {
+http.createServer((request, response)=> {
   response.writeHead(200, {'Content-Type': 'text/plain'});
   response.end('Welcome to my server!\n');
 }).listen(8080);
 
 console.log('My first Node test server is running on Port 8080.');
+
