51451978fd18073495197280cbe04d8d6c26ad86
Codi <cjrobards@gmail.com>
2023-08-17 18:21:30 -0400

    Task 2.4
    -adds morgan middleware
    -adds error handler
    -adds movie list json

--- index.js
+++ index.js
@@ -1,13 +1,33 @@
 const express= require('express');
-const app= express();
 const http= require('http');
-const morgan= require('morgan');
+const app= express();
 const port= 8080;
+const morgan= require('morgan');
+const server= http.createServer(app);
 
-http.createServer((request, response)=> {
-  response.writeHead(200, {'Content-Type': 'text/plain'});
-  response.end('Welcome to my server!\n');
-}).listen(8080);
 
-console.log('My first Node test server is running on Port 8080.');
+server.listen(port, () => {
+  console.log(`Welcome to my server`);
+});
 
+app.use(morgan('combined'));
+
+app.use((err, req, res, next)=> {
+  console.error('Unexpected Error', err.stack);
+  res.status(500).send('Unexpected Error');
+});
+
+app.get('/', (req, res)=> {
+  console.log('Welcome to the home page');
+});
+
+app.get('/movies', (req, res)=> {
+  const tenMovies= [
+    { name: 'example', year: 2010 },
+    { name: 'example2', year: 2011 },
+    { name: 'example3', year: 2012 }
+  ];
+  res.json(tenMovies);
+});
+
+app.use(express.static('public'));
