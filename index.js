2a13d6082a815eddfb90710fc24aa32d423ef9c6
Codi <cjrobards@gmail.com>
2023-12-04 21:28:51 -0300

    fixes problems

--- index.js
+++ index.js
@@ -170,7 +170,7 @@ app.put('/users/:Username', async (req, res) => {
   })
   .catch((err) => {
     console.error(err);
-    res.status(500).send(‘Error: ’ + err);
+    res.status(500).send('Error:' + err);
   })
 
 });
@@ -200,7 +200,7 @@ app.post('/users/:Username/movies/:MovieID', async (req, res) => {
   })
   .catch((err) => {
     console.error(err);
-    res.status(500).send(‘Error: ’ + err);
+    res.status(500).send('Error:' + err);
   });
 });
 
