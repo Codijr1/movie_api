d0754b1c3358065ef14d8bae6a4123922a925c2e
Codi <cjrobards@gmail.com>
2024-01-11 18:12:27 -0300

    d

--- auth.js
+++ auth.js
@@ -1,6 +1,6 @@
 const jwtSecret='your_jwt_secret';//MUST be same key used in JWTStrategy
-const jwt=require('jsonwebtoken'),
-    passport=require('passport');
+const jwt=require('jsonwebtoken');
+const passport=require('passport');
 require('./passport');//Local passport file
 
 let generateJWTToken=(user) =>{
