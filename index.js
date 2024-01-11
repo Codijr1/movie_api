47076db3208923814f1923257c2a210e926104b6
Codi <cjrobards@gmail.com>
2024-01-11 16:47:46 -0300

    signup
    -changes url endpoint to better reflect its purpose

--- index.js
+++ index.js
@@ -152,7 +152,7 @@ app.get('/directors/:Name', /*passport.authenticate('jwt', { session: false }),*
 //commands to manipulate data
 
 //Add a user
-app.post('/users',[
+app.post('/signup',[
   check('Username', 'Username is required').isLength({min: 5}),
   check('Username', 'Username contains non alphanumeric characters').isAlphanumeric(),
   check('Password', 'Password is required').not().isEmpty(),
