926c02861dc85c12f92c09604df7eb5bb2a80b26
Codi <cjrobards@gmail.com>
2024-01-11 18:31:59 -0300

    updates authentication

--- auth.js
+++ auth.js
@@ -1,32 +1,47 @@
-const jwtSecret='your_jwt_secret';//MUST be same key used in JWTStrategy
-const jwt=require('jsonwebtoken');
-const passport=require('passport');
-require('./passport');//Local passport file
+const jwtSecret = 'your_jwt_secret';
+const jwt = require('jsonwebtoken');
+const passport = require('passport');
+const bcrypt = require('bcrypt'); // Import bcrypt
 
-let generateJWTToken=(user) =>{
-    return jwt.sign(user,jwtSecret, {
-        subject:user.Username,//This is the username youre encoding in the JWT
-        expiresIn:'7d',//Specifies time before token expires
-        algorithm:'HS256'//This is used to "sign" or encode the values of the JWT
-    });
-}
-//POST to login
-module.exports=(router) =>{
-    router.post('/login', (req,res) =>{
-        passport.authenticate('local',{session:false},(error,user,info) =>{
-            if (error || !user){
-                return res.status(400).json({
-                    message:'Something is not right',
-                    user: user
-                });
-            }
-            req.login(user,{session:false},(error) =>{
-                if (error){
-                    res.send(error);
-            }
-            let token=generateJWTToken(user.toJSON());
-            return res.json({user,token});
-            });
-        })(req,res);
-    });
-}
+require('./passport'); // Local passport file
+
+let generateJWTToken = (user) => {
+  return jwt.sign(user, jwtSecret, {
+    subject: user.Username,
+    expiresIn: '7d',
+    algorithm: 'HS256',
+  });
+};
+
+module.exports = (router) => {
+  router.post('/login', async (req, res) => {
+    passport.authenticate('local', { session: false }, async (error, user, info) => {
+      if (error || !user) {
+        return res.status(400).json({
+          message: 'Something is not right',
+          user: user,
+        });
+      }
+
+      try {
+        // Compare the user's provided password with the hashed password in the database
+        const passwordMatch = await bcrypt.compare(req.body.secret, user.Password);
+
+        if (!passwordMatch) {
+          return res.status(401).json({ message: 'Invalid credentials' });
+        }
+
+        req.login(user, { session: false }, (error) => {
+          if (error) {
+            res.send(error);
+          }
+
+          let token = generateJWTToken(user.toJSON());
+          return res.json({ user, token });
+        });
+      } catch (bcryptError) {
+        return res.status(500).json({ message: 'Internal server error' });
+      }
+    })(req, res);
+  });
+};
