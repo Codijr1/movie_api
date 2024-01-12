c01efc1fd23526872687c9b0c31d3a502b7bf475
Codi <cjrobards@gmail.com>
2024-01-12 00:21:51 -0300

    Update passport.js

--- passport.js
+++ passport.js
@@ -1,30 +1,40 @@
-passport.use(
-    new LocalStrategy(
-        {
-            usernameField: 'Username', // Specify the field name for the username
-            passwordField: 'Password', // Specify the field name for the password
-        },
-        async (username, password, callback) => {
-            console.log(`Attempting login with: ${username} ${password}`);
-            try {
-                const user = await Users.findOne({ Username: username });
-                
-                if (!user) {
-                    console.log('Username not found');
-                    return callback(null, false, { message: 'Incorrect username or password' });
-                }
+const passport=require('passport'),
+    LocalStrategy=require('passport-local'),
+    Models=require('./models.js'),
+    passportJWT=require('passport-jwt');
 
-                if (!user.validatePassword(password)) {
-                    console.log('Incorrect password');
-                    return callback(null, false, { message: 'Incorrect password.' });
-                }
+let Users=Models.User,
+    JWTStrategy=passportJWT.Strategy,
+    ExtractJWT=passportJWT.ExtractJwt;
 
-                console.log('Login successful');
-                return callback(null, user);
-            } catch (error) {
-                console.log('Error during login:', error);
-                return callback(error);
+    passport.use(
+        new LocalStrategy(
+            {
+                usernameField: 'Username', // Specify the field name for the username
+                passwordField: 'Password', // Specify the field name for the password
+            },
+            async (username, password, callback) => {
+                console.log(`Attempting login with: ${username} ${password}`);
+                try {
+                    const user = await Users.findOne({ Username: username });
+                    
+                    if (!user) {
+                        console.log('Username not found');
+                        return callback(null, false, { message: 'Incorrect username or password' });
+                    }
+    
+                    if (!user.validatePassword(password)) {
+                        console.log('Incorrect password');
+                        return callback(null, false, { message: 'Incorrect password.' });
+                    }
+    
+                    console.log('Login successful');
+                    return callback(null, user);
+                } catch (error) {
+                    console.log('Error during login:', error);
+                    return callback(error);
+                }
             }
-        }
-    )
-);
+        )
+    );
+    
