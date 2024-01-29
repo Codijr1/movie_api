6edef0f888d9f55521be087f0cb3893cd6ef361a
Codi <cjrobards@gmail.com>
2024-01-29 14:48:15 -0300

    bug fix

--- passport.js
+++ passport.js
@@ -1,40 +1,60 @@
-const passport=require('passport'),
-    LocalStrategy=require('passport-local'),
-    Models=require('./models.js'),
-    passportJWT=require('passport-jwt');
+const passport = require('passport'),
+    LocalStrategy = require('passport-local'),
+    Models = require('./models.js'),
+    passportJWT = require('passport-jwt');
 
-let Users=Models.User,
-    JWTStrategy=passportJWT.Strategy,
-    ExtractJWT=passportJWT.ExtractJwt;
+let Users = Models.User,
+    JWTStrategy = passportJWT.Strategy,
+    ExtractJWT = passportJWT.ExtractJwt;
 
-    passport.use(
-        new LocalStrategy(
-            {
-                usernameField: 'Username',
-                passwordField: 'Password'
-            },
-            async (username, password, callback) => {
-                console.log(`Attempting login with: ${username} ${password}`);
-                try {
-                    const user = await Users.findOne({ Username: username });
-                    
-                    if (!user) {
-                        console.log('Username not found');
-                        return callback(null, false, { message: 'Incorrect username or password' });
-                    }
-    
-                    if (!user.validatePassword(password)) {
-                        console.log('Incorrect password');
-                        return callback(null, false, { message: 'Incorrect password.' });
-                    }
-    
-                    console.log('Login successful');
-                    return callback(null, user);
-                } catch (error) {
-                    console.log('Error during login:', error);
-                    return callback(error);
+passport.use(
+    new LocalStrategy(
+        {
+            usernameField: 'Username',
+            passwordField: 'Password'
+        },
+        async (username, password, callback) => {
+            console.log(`Attempting login with: ${username} ${password}`);
+            try {
+                const user = await Users.findOne({ Username: username });
+
+                if (!user) {
+                    console.log('Username not found');
+                    return callback(null, false, { message: 'Incorrect username or password' });
                 }
+
+                if (!user.validatePassword(password)) {
+                    console.log('Incorrect password');
+                    return callback(null, false, { message: 'Incorrect password.' });
+                }
+
+                console.log('Login successful');
+                return callback(null, user);
+            } catch (error) {
+                console.log('Error during login:', error);
+                return callback(error);
             }
-        )
-    );
-    
+        }
+    )
+);
+
+passport.use(
+    new JWTStrategy(
+        {
+            jwtFromRequest: ExtractJWT.fromAuthHeaderAsBearerToken(),
+            key: 'key'
+        },
+        async (jwtPayload, callback) => {
+            try {
+                const user = await Users.findById(jwtPayload._id);
+                if (!user) {
+                    return callback(null, false, { message: 'User not found' });
+                }
+                return callback(null, user);
+            } catch (error) {
+                console.error('Error in JWT authentication:', error);
+                return callback(error, false);
+            }
+        }
+    )
+);
