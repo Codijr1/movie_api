df4e0079ab568cb2e06a9eb28533639165d5879b
Codi <cjrobards@gmail.com>
2024-02-04 21:45:38 -0300

    j

--- index.js
+++ index.js
@@ -3,7 +3,6 @@ const app = express();
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 const cors = require('cors');
-const cors = require('cors');
 let allowedOrigins = ['http://localhost:1234', 'https://myflixproject-9c1001b14e61.herokuapp.com', 'https://myflixprojectcjr.netlify.app'];
 app.use(cors({
   origin: (origin, callback) => {
