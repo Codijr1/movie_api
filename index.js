8fa0a366436f5006818cdc0c3da156c4dbb63b78
Codi <cjrobards@gmail.com>
2024-02-04 21:38:11 -0300

    CORS

--- index.js
+++ index.js
@@ -3,7 +3,8 @@ const app = express();
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 const cors = require('cors');
-let allowedOrigins = ['http://localhost:1234', 'https://myflixproject-9c1001b14e61.herokuapp.com/', 'https://myflixprojectcjr.netlify.app/'];
+const cors = require('cors');
+let allowedOrigins = ['http://localhost:1234', 'https://myflixproject-9c1001b14e61.herokuapp.com', 'https://myflixprojectcjr.netlify.app'];
 app.use(cors({
   origin: (origin, callback) => {
     if (!origin) return callback(null, true);
